a96f813963106b0b54427d824320e49cf793ea30
Satya Prakash Sahu <satyasahu79@gmail.com>
2021-09-25 10:03:11 +0530

    Initial Commit

---
+++ MovieTicketDribble/MovieTicketDribbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTicketDribbleApp.swift
+//  MovieTicketDribble
+//
+//  Created by Satya Prakash Sahu on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieTicketDribbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
